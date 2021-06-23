a40def87051a4367c2c11659a0c5bbfb68cfe13e
vamshiboju <83817973+vamshiboju@users.noreply.github.com>
2021-06-23 09:08:56 +0530

    Update settings.py

--- CalorieCounter/settings.py
+++ CalorieCounter/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path , os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
