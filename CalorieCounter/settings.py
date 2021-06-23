921a46dab5b0b357c20863c761b5ad92225861f8
vamshiboju <83817973+vamshiboju@users.noreply.github.com>
2021-06-23 11:21:16 +0530

    Update settings.py

--- CalorieCounter/settings.py
+++ CalorieCounter/settings.py
@@ -128,7 +128,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
-    os.path.join(BASE_DIR,'media')
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 django_heroku.settings(locals())
